c7602f0013306aa69fc4eaddaa6ce7e63580ae57
arshwaseem <arsh.waseem1@gmail.com>
2025-08-15 15:00:52 +0400

    Added starter spring projects

---
+++ AdderMS/src/test/java/com/arshwaseem/oe_calc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arshwaseem.oe_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
