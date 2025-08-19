246e3251fabc6678b4db341c213ef05a988baaff
arshw <arsh.waseem1@gmail.com>
2025-08-19 13:33:07 +0400

    Removed residual User Files from Adder Folder

--- AdderMS/src/main/java/com/arshwaseem/oe_calc/UserUseCases.java
+++
@@ -1,9 +0,0 @@
-package com.arshwaseem.oe_calc;
-
-import java.util.Optional;
-
-public interface UserUseCases {
-    Optional<User> GetByID(Long id);
-    Optional<User> GetByName(String name);
-    void AddUser (User user);
-}
