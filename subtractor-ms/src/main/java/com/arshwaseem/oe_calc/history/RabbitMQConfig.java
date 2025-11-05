433a1467cda0d57e2eaca9c4a8ed427b90114d3a
arshw <arsh.waseem1@gmail.com>
2025-11-05 19:37:22 +0400

    Applied app level optimizations to subtractor-ms

--- subtractor-ms/src/main/java/com/arshwaseem/oe_calc/history/RabbitMQConfig.java
+++ subtractor-ms/src/main/java/com/arshwaseem/oe_calc/history/RabbitMQConfig.java
@@ -1,10 +1,15 @@
 package com.arshwaseem.oe_calc.history;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 public class RabbitMQConfig {
@@ -19,5 +24,33 @@ public class RabbitMQConfig {
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+
+        template.setMessageConverter(messageConverter());
+
+        template.setMandatory(true);
+
+        RetryTemplate  retryTemplate = new RetryTemplate();
+
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(3);
+
+        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+
+        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+        backOffPolicy.setInitialInterval(1000);
+        backOffPolicy.setMultiplier(2.0);
+        backOffPolicy.setMaxInterval(10000);
+
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+
+        template.setRetryTemplate(retryTemplate);
+
+        return template;
     }
 }
